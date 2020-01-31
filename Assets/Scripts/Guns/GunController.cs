3324e07e510ea71aadf417e98ab70c8b97c000da
Arsalaan <Originator99>
2020-02-01 00:59:02 +0530

    shooting try

---
+++ Assets/Scripts/Guns/GunController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour, IGun {
+    public Transform shoot_point;
+    public GunSO _gun;
+
+    private float fireCooldown;
+
+    private void Update() {
+        if (fireCooldown > 0) {
+            fireCooldown -= Time.deltaTime;
+        }
+    }
+
+    public void Shoot() {
+        if (_gun == null || shoot_point == null) {
+            Debug.LogError("Shoot point or _gun is null, cannot shoot for object name : " + transform.name);
+            return;
+        }
+        if (fireCooldown <= 0) {
+            fireCooldown = _gun.firerate;
+            GameObject bullet = Instantiate(_gun.bullet_prefab, shoot_point.position, Quaternion.identity);
+            //bullet.transform.SetParent(transform.parent.parent);
+
+        }
+    }
+
+    public void setGun(GunSO gunSO) {
+        _gun = gunSO;
+    }
+}
